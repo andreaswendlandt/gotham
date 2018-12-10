63e3316b46acc23ec6986df81df917b5e1fc04a3
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2018-12-10 22:14:41 +0100

    database credentials and connect function via mysqli

---
+++ cronguard/db.inc.php
@@ -0,0 +1,8 @@
+<?php
+// Connecting to the database with the following credentials("host","user","password","database")
+$conn = @(new mysqli("localhost", "cronguard", "superstrong_and_secure_password", "cronguard"));
+if ($conn->connect_error) {
+  echo "Error while connecting: " . mysqli_connect_error() . "<hr />";
+  exit();
+}
+?>
