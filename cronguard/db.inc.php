c531f36d54d9f20ce6538344c2a842a5aeb6e1e9
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2019-01-13 21:34:03 +0100

    removed useless html tag

--- cronguard/db.inc.php
+++ cronguard/db.inc.php
@@ -2,7 +2,7 @@
 // Connecting to the database with the following credentials("host","user","password","database")
 $conn = @(new mysqli("localhost", "cronguard", "superstrong_and_secure_password", "cronguard"));
 if ($conn->connect_error) {
-  echo "Error while connecting: " . mysqli_connect_error() . "<hr />";
+  echo "Error while connecting: " . mysqli_connect_error();
   exit();
 }
 ?>
