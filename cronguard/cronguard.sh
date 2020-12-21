35c11e2967be4f250681b7a563f2ce0203425310
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-12-21 14:52:05 +0100

    Update cronguard.sh

--- cronguard/cronguard.sh
+++ cronguard/cronguard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # author: guerillatux
 # desc: Cronguard Daemon, checks and processes Database Entries, sends Mails and removes them
-# last modified: 07.08.2020
+# last modified: 21.12.2020
 
 # Quit if not called by root
 if [ "$(id -u)" -ne "0" ]; then
